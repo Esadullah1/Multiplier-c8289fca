c662a02f53173363ce93dd1433f0c91bb8ced522
Esadullah Bagci <2083226@talnet.nl>
2020-06-11 11:07:47 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php 
+
+
+
+        $line1 = readline("ik ga twee getalen met elkaar vermenigvuldigen.");
+	 	$line2 = readline("Wat is het eerste getal? ");
+        $line3 = readline("Wat is het tweede getal? ");
+        $line4 = readline("Het antwoord is: $line5"); 
+        $line5 = readline($line2 * $line3);
+        readline_add_history($line);
+
+
+
+
+
+
+	 ?>
+
+
+
+
+</body>
+</html>
