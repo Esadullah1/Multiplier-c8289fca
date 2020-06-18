12418ead50cf1e4366d5479cace1d8300e29092d
Esadullah Bagci <2083226@talnet.nl>
2020-06-18 12:09:02 +0200

    Uitwerking van de exercise

--- multiplier.php
+++ multiplier.php
@@ -1,20 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-  <?php
+
+ <?php
 
 
-      $line1 = readline("Ik ga twee getallen met elkaar vermenigvuldigen ");
+    $line1 = readline("Ik ga twee getallen met elkaar vermenigvuldigen ");
     $line2 = readline("Eerste getal? ");
     $line3 = readline("Tweede getal? ");
-    $line4 = readline("Uw resultaat is: ");
-    $uitkomst = readline($line2 * $line3);
-    readline_add_history($line);
+    echo"Uw resultaat is: "; $uitkomst = readline($line2 * $line3);
+    
 
 
 ?>
-</body>
-</html>
